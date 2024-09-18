277185c69ea6e0d7a782b3c0ac7b8805bb442396
Saugat Kumal <62896600+kumalsaugat@users.noreply.github.com>
2024-09-18 09:21:07 +0545

    :seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        \DB::table('users')->insert(array (
+            0 =>
+                array (
+                    'id' => 1,
+                    'name' => 'Saugat Kumal',
+                    'email' => 'saugatkumal452@gmail.com',
+                    'password' => bcrypt('123456789'),
+                    'remember_token' => '$2y$12$OHoGL5SvqLI927igkZHMmOTwkEoeDSOnzUmJfybifG4DnZ3alYrja',
+                    'created_at' => '2024-09-12 12:48:42',
+                    'updated_at' => '2024-09-12 12:48:42',
+                ),
+        ));
     }
 }
