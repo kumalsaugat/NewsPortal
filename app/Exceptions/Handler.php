fca420499c07ec07e6c88869f53dd65280b2a9b0
Saugat Kumal <62896600+kumalsaugat@users.noreply.github.com>
2024-10-01 15:41:44 +0545

    :return redirect to 404 page if page not found

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,29 @@
+<?php
+// app/Exceptions/Handler.php
+
+namespace App\Exceptions;
+
+use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class Handler extends ExceptionHandler
+{
+    // Other code...
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Handle 404 error
+        if ($exception instanceof NotFoundHttpException) {
+            return redirect()->route('dashboard');
+        }
+
+        return parent::render($request, $exception);
+    }
+}
