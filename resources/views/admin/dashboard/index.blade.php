8094c43ae0051f1c32d6979ba3d9626e47226092
Saugat Kumal <62896600+kumalsaugat@users.noreply.github.com>
2024-10-01 12:42:13 +0545

    :error solve on user image and dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,8 +5,7 @@
         <div class="container-fluid"> <!--begin::Row-->
             <div class="row">
                 <div class="col-sm-6">
-                    {{-- <h3 class="mb-0">{{ Breadcrumbs::render('dashboard') }}</h3> --}}
-                    {{ Breadcrumbs::render('dashboard') }}
+                    <h3 class="mb-0">{{ Breadcrumbs::render('dashboard') }}</h3>
                 </div>
             </div> <!--end::Row-->
         </div> <!--end::Container-->
--- resources/views/admin/user/field.blade.php
+++ resources/views/admin/user/field.blade.php
@@ -4,7 +4,8 @@
         <div class="col-md-6">
             {{-- Username Field --}}
             <div class="mb-3">
-                <label for="name" class="form-label"><strong>@lang('app.user.name'):<span class="text-danger">*</span></strong></label>
+                <label for="name" class="form-label"><strong>@lang('app.user.name'):<span
+                            class="text-danger">*</span></strong></label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name"
                     value="{{ old('name', $userData->name ?? '') }}">
                 @error('name')
@@ -15,8 +16,10 @@
             {{-- Password Field --}}
             @if (!$isEdit)
                 <div class="mb-3">
-                    <label for="password" class="form-label"><strong>@lang('app.user.password'):<span class="text-danger">*</span></strong></label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
+                    <label for="password" class="form-label"><strong>@lang('app.user.password'):<span
+                                class="text-danger">*</span></strong></label>
+                    <input type="password" class="form-control" id="password" name="password"
+                        placeholder="Enter Password">
                     @error('password')
                         <div class="form-text text-danger">{{ $message }}</div>
                     @enderror
@@ -25,7 +28,8 @@
 
             {{-- Phone Field --}}
             <div class="mb-3">
-                <label for="phone" class="form-label"><strong>@lang('app.user.phone'):<span class="text-danger">*</span></strong></label>
+                <label for="phone" class="form-label"><strong>@lang('app.user.phone'):<span
+                            class="text-danger">*</span></strong></label>
                 <input type="text" class="form-control" id="phone" name="phone" placeholder="Enter Phone"
                     value="{{ old('phone', $userData->phone ?? '') }}">
                 @error('phone')
@@ -38,9 +42,10 @@
         <div class="col-md-6">
             {{-- Email Field --}}
             <div class="mb-3">
-                <label for="email" class="form-label"><strong>@lang('app.user.email'):<span class="text-danger">*</span></strong></label>
+                <label for="email" class="form-label"><strong>@lang('app.user.email'):<span
+                            class="text-danger">*</span></strong></label>
                 <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email"
-                    value="{{ old('email', $userData->email ?? '') }}" @if(isset($userData)) readonly @endif>
+                    value="{{ old('email', $userData->email ?? '') }}">
                 @error('email')
                     <div class="form-text text-danger">{{ $message }}</div>
                 @enderror
@@ -52,6 +57,7 @@
                 <div class="input-group mb-3">
                     <input type="file" class="form-control" id="image" name="image">
                 </div>
+                <input type="hidden" name="uploaded_image" value="{{ old('uploaded_image') }}">
                 @error('image')
                     <div class="form-text text-danger">{{ $message }}</div>
                 @enderror
@@ -68,10 +74,10 @@
                         <strong>@lang('app.news.status'):<span class="text-danger"> *</span></strong>
                     </label>
                     <div class="form-check form-switch">
-                        <input class="form-check-input @error('status') is-invalid @enderror" type="checkbox" role="switch"
-                            id="status" name="status" value="1"
+                        <input class="form-check-input @error('status') is-invalid @enderror" type="checkbox"
+                            role="switch" id="status" name="status" value="1"
                             {{ (isset($userData) && $userData->status) || old('status') ? 'checked' : '' }}
-                            onchange="toggleStatusLabel()" >
+                            onchange="toggleStatusLabel()">
                         <label class="form-check-label" for="status" id="statusLabel">
                             {{ (isset($userData) && $userData->status) || old('status') ? 'Active' : 'Inactive' }}
                         </label>
